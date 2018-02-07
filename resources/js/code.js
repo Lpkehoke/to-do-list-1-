997e57bd533a1f1fbe1fcc9cc641ad996f1d349d
alex <ibymif2000@yandex.ru>
2018-02-08 00:50:34 +0300

    some dragon-x

--- resources/js/code.js
+++ resources/js/code.js
@@ -4,6 +4,8 @@ window.onload = function () {
 	let WRAPPER = document.querySelector('.wrapper');
 	let ALL_DATA = {};
 	let COUNTER = 0;
+	let WIDTH_COLUMN = 15 * 16;
+	let MARGIN_NOTE = 16;
 
 	let firstGenerator = Block('column', 'Creating generator', true);
 	WRAPPER.appendChild(firstGenerator);
@@ -44,32 +46,107 @@ window.onload = function () {
 
 			mainObj = note;
 
-			parent = document.querySelector(`[data-id-in-relativs="${note.dataset.parent}"]`);
+			parent = document.querySelector(`[data-id-in-relativs="${note.dataset.parent}"]`).querySelector('.list');
 		};
 		/***/
-		if (!first) {
+		if (!first) { // to do
 			mainObj.onmousedown = function(e) {
-
 				if (e.target !== mainObj) return;
+
+				let emptyDiv = document.createElement('div');
+				let startLeft = mainObj.getBoundingClientRect().left;
+
+				let startTop = mainObj.getBoundingClientRect().top;
+
+				emptyDiv.className = (type === 'column') ? 'emptyColumn' : 'emptyNote';
+
+				parent.insertBefore(emptyDiv , mainObj);
 
 				mainObj.style.position = 'absolute';
 				mainObj.style.zIndex = 1000;
-				moveAt(e);
 
+				xControle(e);
+				if (type === 'note') yControle(e);
 				parent.appendChild(mainObj);
 
 				document.onmousemove = function(e) {
-					moveAt(e);
+					xControle(e);
+					if (type === 'note') yControle(e , parent);
 				};
+
 				mainObj.onmouseup = function() {
 					document.onmousemove = null;
 					mainObj.onmouseup = null;
-					mainObj.position = 'relative';
+					mainObj.style.position = 'relative';
 					mainObj.style.zIndex = 1;
+					mainObj.style.left = 0;
+					mainObj.style.top = 0;
+					parent.insertBefore(mainObj , emptyDiv);
+					emptyDiv.remove();
 				};
-				function moveAt(e) {
+
+				function xControle(e) {
+
+					if (e.pageX < startLeft && e.pageX > 16) {
+						let previous = emptyDiv.previousElementSibling;
+
+						emptyDiv.remove();
+						if (type === 'column') parent.insertBefore(emptyDiv, previous);
+						else if (type === 'note') {
+							let previousList = parent.parentElement.previousElementSibling.querySelector('.list');
+							previousList.appendChild(mainObj);
+							yControle (e, previousList, true);
+						};
+
+						startLeft -= WIDTH_COLUMN;
+					} else if (e.pageX > startLeft + WIDTH_COLUMN && ( type === 'note' || emptyDiv.nextElementSibling.nextElementSibling.nextElementSibling)) {
+						let post = emptyDiv.nextElementSibling.nextElementSibling;
+
+						emptyDiv.remove();
+						if (type === 'column') parent.insertBefore(emptyDiv, post);
+						else if (type === 'note') {
+							let postList = parent.parentElement.nextElementSibling.querySelector('.list');
+							postList.appendChild(mainObj);
+							yControle (e, postList, true);
+						};
+
+						startLeft += WIDTH_COLUMN;
+					};
+
 					mainObj.style.left = e.pageX - mainObj.offsetWidth / 2 + 'px';
 					mainObj.style.top = e.pageY - mainObj.offsetHeight / 2 + 'px';
+				};
+				function yControle (e, list, fromX = false) {
+					if (fromX) {
+						parent = list;
+						let find = false;
+						for (let i = 0; i < parent.children.length; i++) {
+							console.log(parent.children[i].getBoundingClientRect().top);
+							if (e.pageY < parent.children[i].getBoundingClientRect().top) {
+								parent.insertBefore(emptyDiv , parent.children[i - 1]);
+								break;
+								find = true;
+							};
+						};
+						if (!find) parent.appendChild(emptyDiv);
+						mainObj.dataset.parent = parent.parentElement.dataset.idInRelativs;
+					};
+
+					if (e.pageY < startTop && emptyDiv.previousElementSibling) {
+						let previous = emptyDiv.previousElementSibling;
+
+						emptyDiv.remove();
+						parent.insertBefore(emptyDiv, previous);
+
+						startTop -= (previous.offsetHeight + MARGIN_NOTE / 2);
+					} else if (e.pageY > startTop + mainObj.offsetHeight + MARGIN_NOTE / 2 && emptyDiv.nextElementSibling.nextElementSibling) {
+						let post = emptyDiv.nextElementSibling.nextElementSibling;
+
+						emptyDiv.remove();
+						parent.insertBefore(emptyDiv, post);
+
+						startTop += (post.offsetHeight + MARGIN_NOTE / 2);
+					};
 				};
 			};
 		};
@@ -93,6 +170,7 @@ window.onload = function () {
 				else {
 					parent.appendChild(newBlock);
 					ALL_DATA[count].push(newBlock);
+					textarea.value = '';
 				};
 			};
 
